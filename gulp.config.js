90491f4a88d612cf710c31c633ee040c6567bb0f
Joe Robles <joe.robles.pdj@gmail.com>
2015-09-14 23:06:26 -0500

    removed ignorePath

--- gulp.config.js
+++ gulp.config.js
@@ -29,7 +29,7 @@ module.exports = function() {
             options: {
                 module: 'app.core',
                 standAlone: false,
-                root: 'app/'
+                root: ''
             }
         }
     };
